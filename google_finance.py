10d75c95807af784287bd7b95b3dadf4a2282cf6
Lee Barnard <lee.barnard@gmail.com>
2018-02-20 17:34:56 +0000

    generic graphing and more sample data files

--- google_finance.py
+++ google_finance.py
@@ -1,54 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
-import datetime
 
 
 class GoogleFinance(object):
 
     def plot(self):
-        data = sp.genfromtxt(r"data\EMG.csv", delimiter=",", usecols=(0, 1, 2, 3, 4, 5),
-                             skip_header=1, dtype=int)
+        self.getdataandplot("EMG",2)
+        self.getdataandplot("FTSE",2)
+
+    def getdataandplot(self, source, idx):
+        datasource = self.getdata(r"data\\" + source + ".csv")
+        datasource = np.reshape(datasource, (datasource.__len__(), 6))  # [::-1]
+        datasource = self.addidxcolumn(datasource, 1)
+        x, y = self.splitintoxy(datasource, idx)
+        self.plotit(x, y, source)
+        self.linspaceit(x, y, 10, 'red')
+        plt.savefig("D:\\" + source + ".png")
+        plt.clf()
+
+    def getdata(self, filename):
+        print(filename)
+        return sp.genfromtxt(filename, delimiter=",", usecols=(0, 1, 2, 3, 4, 5), skip_header=1, dtype=int)
+
+    def addidxcolumn(self, data, seed):
+        print(data)
         days = data.__len__()
-        print(days)
-        data90 = np.reshape(data, (days, 6))#[::-1]
-        x = np.arange(days)
-        x90 = np.reshape(x, (days, 1))
-        print(data90.shape)
-        print(x90.shape)
-        data2 = np.hstack((x90, data90))
-        print(data2)
-        x = data2[:, 0]
-        y = data2[:, 2]
+        idx = np.arange(seed-1, days+(seed-1))
+        idx = np.reshape(idx, (days, 1))
+        return np.hstack((idx, data))
+
+    def plotit(self, x, y, label):
+        plt.plot(x, y)
+        divisor = max(x) / 7
+        plt.xticks(np.arange(min(x), max(x) + 1, divisor), np.arange(min(x) / divisor, (max(x) + 1) / divisor, 1))
+        plt.legend([label], loc="upper left")
+        plt.grid(True, linestyle='-', color='0.75')
+
+    def splitintoxy(self, data, idx):
+        x = data[:, 0]
+        y = data[:, idx]
         x = x[~sp.isnan(y)]
         y = y[~sp.isnan(y)]
+        return x, y
 
-        plt.scatter(x, y, s=2)
-        plt.xticks(np.arange(min(x), max(x) + 1, 365), np.arange(min(x) / 365, (max(x) + 1) / 365, 1))
-        plt.legend(["EMG.L"], loc="upper left")
-        plt.grid(True, linestyle='-', color='0.75')
-        plt.savefig("D:\plot.png")
-
-        fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
-
-        def error(f, x, y):
-            return sp.sum((f(x) - y) ** 2)
-
-        print("Model parameters: %s" % fp1)
-        print(residuals)
-        f1 = sp.poly1d(fp1)
-        print(error(f1, x, y))
-
+    def linspaceit(self, x, y, deg, color):
         fx = sp.linspace(0, x[-1], 1000)  # generate X-values for plotting
-        plt.plot(fx, f1(fx), linewidth=2, color='green')
-        plt.savefig("D:\plot_1.png")
-
-        f2p = sp.polyfit(x, y, 2)
-        f2 = sp.poly1d(f2p)
-        plt.plot(fx, f2(fx), linewidth=2, color='yellow')
-        plt.savefig("D:\plot_2.png")
-
-        f10p = sp.polyfit(x, y, 10)
+        f10p = sp.polyfit(x, y, deg)
         f10 = sp.poly1d(f10p)
-        plt.plot(fx, f10(fx), linewidth=2, color='red')
-        plt.savefig("D:\plot_3.png")
+        plt.plot(fx, f10(fx), linewidth=2, color=color)
+
+
--- test_google_finance.py
+++ test_google_finance.py
@@ -5,5 +5,21 @@ from google_finance import GoogleFinance
 class TddInPythonExample_Plotter(unittest.TestCase):
 
     def test_savepng(self):
-        googlefinance = GoogleFinance()
-        googlefinance.plot()
+        gf = GoogleFinance()
+        gf.plot()
+
+    def test_addidxcolumn(self):
+        gf = GoogleFinance()
+        reshaped = gf.addidxcolumn([
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407],
+            [-1, 240, 239, 240, 231, 3260407]
+        ],1)
+        self.assertEqual(reshaped.shape, (9, 7))
+        self.assertEqual(reshaped[5,0], 5)
